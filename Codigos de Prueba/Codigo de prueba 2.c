613eda7ae991b05b787963c1b27979b17ea7d31a
HaroldMateo22 <20221719@itla.edu.do>
2022-10-05 23:10:31 -0400

    adicion del 2do ejemplo

---
+++ Codigos de Prueba/Codigo de prueba 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main (void)
+{
+    float pre, npr;
+    printf ("Ingrese el precio del producto: ");
+    scanf ("%f", &pre);
+if (pre < 1500)
+     npr = pre * 1.11;
+    else
+     npr = pre * 1.08;
+printf ("\nNuevo precio del producto: %8.2f", npr);
+system ("pause");
+return("0");
+}
